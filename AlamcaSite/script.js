057fd3fa40fba03e5bc3d26e24c50a966b16fece
workingspace84-png <workingspace84@gmail.com>
2025-12-29 17:25:36 +0400

    Delete AlamcaSite directory

--- AlamcaSite/script.js
+++
@@ -1,9 +0,0 @@
-function checkAntwort(antwort) {
-  const richtig = "der"; // doğru cevap
-  const ergebnis = document.getElementById("ergebnis");
-  if (antwort === richtig) {
-    ergebnis.textContent = "Richtig!";
-  } else {
-    ergebnis.textContent = "Falsch!";
-  }
-}
