be637ceaecab61264f9cc3b5910934b8970cf1de
workingspace84-png <workingspace84@gmail.com>
2026-01-08 18:32:53 +0400

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const menuIcon = document.querySelector('.menu-icon');
   const menu = document.getElementById('menu');
-
   if (menuIcon && menu) {
     menuIcon.addEventListener('click', () => {
       menu.classList.toggle('hidden');
